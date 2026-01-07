9d830f45735968426f8794e1db5e0a65961fa938
towertemennigru <a31zturk@gmail.com>
2026-01-07 03:42:31 +0300

    Create next.js

---
+++ next.js
@@ -0,0 +1,7 @@
+npm install @solana/web3.js \
+    @solana/wallet-adapter-base \
+    @solana/wallet-adapter-react \
+    @solana/wallet-adapter-react-ui \
+    @solana/wallet-adapter-wallets \
+    @farcaster/frame-sdk \
+    lucide-react
