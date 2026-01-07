8386247d9609f57a44a30dcbbf558194071c0c73
towertemennigru <a31zturk@gmail.com>
2026-01-07 03:45:21 +0300

    Delete next.js

--- next.js
+++
@@ -1,7 +0,0 @@
-npm install @solana/web3.js \
-    @solana/wallet-adapter-base \
-    @solana/wallet-adapter-react \
-    @solana/wallet-adapter-react-ui \
-    @solana/wallet-adapter-wallets \
-    @farcaster/frame-sdk \
-    lucide-react
